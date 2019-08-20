009d783c2ba013f982d1eaa895ef41ea0696ea90
Sanyam Dogra <sdlord07@gmail.com>
2019-08-20 22:44:55 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Container, Row, Col, Button } from "design-react-kit";
+import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+import "typeface-titillium-web";
+import "typeface-roboto-mono";
+import "typeface-lora";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-3">
+    <Col>
+    <h1 className='text-center'>Design-react-kit</h1>
+      <h3 className="pt-5 pb-3">Different types of Buttons</h3>
+      <Row>
+        <div className="pr-3">
+        <Button color="primary">Primary</Button>
+        </div>
+        <div className="pr-3">
+        <Button color="primary" outline>Primary Outline</Button>
+        </div>
+        <div className="pr-3">
+        <Button color="secondary">Secondary</Button>
+        </div>
+        <div className="pr-3">
+        <Button color="secondary" outline>Secondary Outline</Button>
+        </div>
+        <div className="pr-3">
+        <Button color="success">Success</Button>
+        </div>
+        <div className="pr-3">
+        <Button color="success" outline>Success Outline</Button>
+        </div>
+      </Row>
+      </Col>  
+    </Container>
   );
 }
 
